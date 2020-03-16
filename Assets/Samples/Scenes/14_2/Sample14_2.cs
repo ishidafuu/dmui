e136910de5aaffd378b8dadbcf037a1177fce735
ishidafuu <ishidafuu@gmail.com>
2020-03-16 18:05:53 +0900

    update

--- Assets/Samples/Scenes/14_2/Sample14_2.cs
+++ Assets/Samples/Scenes/14_2/Sample14_2.cs
@@ -9,7 +9,6 @@
 // 押したボタンに応じたログを表示します。
 
 using DMUIFramework.Samples;
-using UniRx.Async;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,26 +20,6 @@ namespace DM
         {
             UIController.SetImplement(new PrefabLoader(), new Sounder(), new FadeCreator());
             UIController.Instance.AddFront(new Sample14_2Scene());
-        }
-    }
-
-    class Sample14_2CellViewButton : UIPart
-    {
-        private readonly CellView14_2 m_cellView14_2;
-
-        public Sample14_2CellViewButton(CellView14_2 cellView14_2, GameObject buttonObject)
-            : base(buttonObject.transform)
-        {
-            m_cellView14_2 = cellView14_2;
-        }
-
-        public override async UniTask OnLoadedPart(UIBase targetLayer) { }
-
-        public override bool OnClick(TouchEvent touch, UISound uiSound)
-        {
-            // TouchListenerを継承してGetComponentせずに済むようなクラスを作ってもいいかも
-            Debug.Log($"{m_cellView14_2.someTextText.text}");
-            return true;
         }
     }
 }
---
+++ Assets/Samples/Scenes/14_2/Sample14_2CellViewButton.cs
@@ -0,0 +1,24 @@
+﻿using UniRx.Async;
+using UnityEngine;
+
+namespace DM {
+    class Sample14_2CellViewButton : UIPart
+    {
+        private readonly CellView14_2 m_cellView14_2;
+
+        public Sample14_2CellViewButton(CellView14_2 cellView14_2, GameObject buttonObject)
+            : base(buttonObject.transform)
+        {
+            m_cellView14_2 = cellView14_2;
+        }
+
+        public override async UniTask OnLoadedPart(UIBase targetLayer) { }
+
+        public override bool OnClick(TouchEvent touch, UISound uiSound)
+        {
+            // TouchListenerを継承してGetComponentせずに済むようなクラスを作ってもいいかも
+            Debug.Log($"{m_cellView14_2.someTextText.text}");
+            return true;
+        }
+    }
+}
