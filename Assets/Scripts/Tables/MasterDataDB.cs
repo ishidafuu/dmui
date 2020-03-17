552be78824676987a8e9c9e6159a43d3880da995
ishidafuu <ishidafuu@gmail.com>
2020-03-17 19:27:45 +0900

    update

---
+++ Assets/Scripts/Tables/MasterDataDB.cs
@@ -0,0 +1,11 @@
+﻿using MasterData;
+using UnityEngine;
+
+namespace DM
+{
+    public class MasterDataDB
+    {
+        private static MemoryDatabase _db = new MemoryDatabase((Resources.Load("master-data") as TextAsset).bytes);
+        public static MemoryDatabase DB => _db;
+    }
+}
--- Assets/Scripts/Tables/TableTest.cs
+++ Assets/Scripts/Tables/TableTest.cs
@@ -10,14 +10,29 @@ namespace DM
     {
         void Start()
         {
-            // try {
-            //     StaticCompositeResolver.Instance.Register(
-            //         MasterMemoryResolver.Instance,
-            //         GeneratedResolver.Instance,
-            //         StandardResolver.Instance
-            //     );
-            // } catch { }
-            
+            Test2();
+            // Test1();
+        }
+
+        private static void Test2()
+        {
+            var db = MasterDataDB.DB;
+
+            foreach (var skill in db.SkillTable.All)
+            {
+                Debug.Log($"Skill Name: {skill.SkillName}");
+
+                var lv = (int)Random.Range(1, 10);
+                Debug.Log($"Skill Lv: {lv}");
+
+                var parameter = db.SkillParameterTable
+                    .FindBySkillIDAndSkillLv((skill.SkillID, lv));
+                Debug.Log($"Skill Damage: {parameter.Damage}");
+            }
+        }
+
+        private static void Test1()
+        {
             // to create database, use DatabaseBuilder and Append method.
             var builder = new DatabaseBuilder();
             builder.Append(new Person[]
@@ -33,40 +48,40 @@ namespace DM
                 new Person {PersonId = 8, Age = 63, Gender = Gender.Female, Name = "Lori Wilson"},
                 new Person {PersonId = 9, Age = 34, Gender = Gender.Female, Name = "Lena Ramsey"},
             });
-    
+
             // build database binary(you can also use `WriteToStream` for save to file).
             byte[] data = builder.Build();
-    
+
             // -----------------------
-    
+
             // for query phase, create MemoryDatabase.
             // (MemoryDatabase is recommended to store in singleton container(static field/DI)).
             var db = new MemoryDatabase(data);
-    
+
             // .PersonTable.FindByPersonId is fully typed by code-generation.
             Person person = db.PersonTable.FindByPersonId(1);
-    
+
             // Multiple key is also typed(***And * **), Return value is multiple if key is marked with `NonUnique`.
             RangeView<Person> result = db.PersonTable.FindByGenderAndAge((Gender.Female, 23));
-    
+
             Debug.Log("result");
             foreach (var p in result)
             {
                 Debug.Log($"{p.Name}");
             }
-    
+
             // Get nearest value(choose lower(default) or higher).
             RangeView<Person> age1 = db.PersonTable.FindClosestByAge(31);
-    
+
             Debug.Log("age1");
             foreach (var p in age1)
             {
                 Debug.Log($"{p.Name}");
             }
-    
+
             // Get range(min-max inclusive).
             RangeView<Person> age2 = db.PersonTable.FindRangeByAge(20, 29);
-    
+
             Debug.Log("age2");
             foreach (var p in age2)
             {
