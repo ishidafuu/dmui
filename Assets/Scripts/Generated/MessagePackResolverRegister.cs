f72d586b5f53311198469f16455eccaad00da615
ishidafuu <ishidafuu@gmail.com>
2020-03-17 16:34:36 +0900

    update

---
+++ Assets/Scripts/Generated/MessagePackResolverRegister.cs
@@ -0,0 +1,26 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using MessagePack.Unity;
+using UnityEngine;
+
+namespace DM
+{
+    public class MessagePackResolverRegister : MonoBehaviour
+    {
+        private void Start()
+        {
+            StaticCompositeResolver.Instance.Register(
+                GeneratedResolver.Instance,
+                UnityResolver.Instance,
+                BuiltinResolver.Instance,
+                AttributeFormatterResolver.Instance,
+                PrimitiveObjectResolver.Instance,
+                StandardResolver.Instance
+            );
+            
+            MessagePackSerializerOptions option =
+                MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
+            MessagePackSerializer.DefaultOptions = option;
+        }
+    }
+}
