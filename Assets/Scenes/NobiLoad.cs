9ce52dd134a6dd8c5cfed6b0d08eaa7ee4579a7f
ishidafuu <ishidafuu@gmail.com>
2020-03-21 15:05:45 +0900

    update

---
+++ Assets/Scenes/NobiLoad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+using UniRx.Async;
+
+public class NobiLoad : MonoBehaviour
+{
+    public Image image;
+    [SerializeField] AssetReference target;
+    private void Start()
+    {
+        // SpriteをロードしてImageに表示する
+        Addressables.LoadAsset<Sprite>(target).Completed += op =>
+        {
+            image.sprite = op.Result;
+        };
+    }
+
+    public async UniTask OnLoadedBase()
+    {
+        // await Addressables.LoadAssetAsync<Sprite>("assetbundle/nobi").Completed += op => { image.sprite = op.Result; };
+    }
+}
