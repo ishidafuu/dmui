f989dc965de56ccec4db3548f2dd7c59d78888f2
ishidafuu <ishidafuu@gmail.com>
2020-03-24 17:43:43 +0900

    update

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public static class Settings
 {
@@ -25,17 +27,38 @@ public static class Settings
     private static readonly List<int> s_MyEnumData = new List<int>();
     private static readonly List<int> s_MyEnum2Data = new List<int>();
 
-    private static void InitList(ICollection<int> list, IEnumerable<TableRow> rows)
+    private static void InitList<T>(ICollection<int> list, IReadOnlyList<TableRow> rows)
     {
         list.Clear();
-        foreach (TableRow row in rows)
+
+        int index = 0;
+        foreach (T value in Enum.GetValues(typeof(T)))
         {
-            list.Add(row.Value);
+            string name = Enum.GetName(typeof(T), value);
+            if (rows[index].Name != name)
+            {
+                Debug.LogError($"Unmatched Name rowName:{name} name:{rows[index].Name}");
+                break;
+            }
+
+            if (index >= rows.Count)
+            {
+                Debug.LogError($"Unmatched count under");
+                break;
+            }
+            
+            list.Add(rows[index].Value);
+            index++;
+        }
+        
+        if (index != rows.Count)
+        {
+            Debug.LogError($"Unmatched count over");
         }
     }
 
-    public static void InitMyEnum(IEnumerable<TableRow> rows) => InitList(s_MyEnumData, rows);
-    public static void InitMyEnum2(IEnumerable<TableRow> rows) => InitList(s_MyEnum2Data, rows);
+    public static void InitMyEnum(List<TableRow> rows) => InitList<MyEnum>(s_MyEnumData, rows);
+    public static void InitMyEnum2(List<TableRow> rows) => InitList<MyEnum2>(s_MyEnum2Data, rows);
     public static int Get(MyEnum key) => s_MyEnumData[(int)key];
     public static int Get(MyEnum2 key) => s_MyEnum2Data[(int)key];
 }
