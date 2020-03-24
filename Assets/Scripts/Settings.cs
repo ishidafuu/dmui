8bf2dbbe94e6b690ef08aa7534e278ca035149dc
ishidafuu <ishidafuu@gmail.com>
2020-03-24 18:00:08 +0900

    update

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,64 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
+using UniRx.Async;
 using UnityEngine;
 
-public static class Settings
+namespace DM
 {
-    public enum MyEnum
+    public static class Settings
     {
-        Asdf,
-        Qwer,
-        Zxcv,
-    }
-
-    public enum MyEnum2
-    {
-        Asdf,
-        Qwer,
-        Zxcv,
-    }
-
-    public class TableRow
-    {
-        public string Name;
-        public int Value;
-    }
-
-    private static readonly List<int> s_MyEnumData = new List<int>();
-    private static readonly List<int> s_MyEnum2Data = new List<int>();
-
-    private static void InitList<T>(ICollection<int> list, IReadOnlyList<TableRow> rows)
-    {
-        list.Clear();
-
-        int index = 0;
-        foreach (T value in Enum.GetValues(typeof(T)))
+        public enum MyEnum
         {
-            string name = Enum.GetName(typeof(T), value);
-            if (rows[index].Name != name)
-            {
-                Debug.LogError($"Unmatched Name rowName:{name} name:{rows[index].Name}");
-                break;
-            }
-
-            if (index >= rows.Count)
-            {
-                Debug.LogError($"Unmatched count under");
-                break;
-            }
-            
-            list.Add(rows[index].Value);
-            index++;
+            Asdf,
+            Qwer,
+            Zxcv,
         }
+
+        public enum MyEnum2
+        {
+            Asdf,
+            Qwer,
+            Zxcv,
+        }
+
+        public class TableRow
+        {
+            public string Name;
+            public int Value;
+        }
+
+        private static readonly List<int> s_MyEnumData = new List<int>();
+        private static readonly List<int> s_MyEnum2Data = new List<int>();
         
-        if (index != rows.Count)
+        public static int Get(MyEnum key) => s_MyEnumData[(int)key];
+        public static int Get(MyEnum2 key) => s_MyEnum2Data[(int)key];
+        
+        private static void InitMyEnum(TableRow[] rows) => InitList<MyEnum>(s_MyEnumData, rows);
+        private static void InitMyEnum2(TableRow[] rows) => InitList<MyEnum2>(s_MyEnum2Data, rows);
+        private static void InitList<T>(ICollection<int> list, TableRow[] rows)
         {
-            Debug.LogError($"Unmatched count over");
+            list.Clear();
+
+            int index = 0;
+            foreach (T value in Enum.GetValues(typeof(T)))
+            {
+                string name = Enum.GetName(typeof(T), value);
+                if (rows[index].Name != name)
+                {
+                    Debug.LogError($"Unmatched Name rowName:{name} name:{rows[index].Name}");
+                    break;
+                }
+
+                if (index >= rows.Length)
+                {
+                    Debug.LogError($"Unmatched count under");
+                    break;
+                }
+
+                list.Add(rows[index].Value);
+                index++;
+            }
+
+            if (index != rows.Length)
+            {
+                Debug.LogError($"Unmatched count over");
+            }
+        }
+
+        public static async UniTask LoadTableAsync()
+        {
+            AirTableClient client = new AirTableClient("key3NNedymjZdyPup");
+            AirTableBase clientBaseMyEnum = client.GetBase("appsj9JjmBwaF3Hbz");
+            AirTableBase clientBaseMyEnum2 = client.GetBase("appsj9JjmBwaF3Hbz");
+            
+            try
+            {
+                TableRow[] allRows = await clientBaseMyEnum.LoadTableAsync<TableRow>();
+                InitMyEnum(allRows);
+                
+                TableRow[] allRows2 = await clientBaseMyEnum2.LoadTableAsync<TableRow>();
+                InitMyEnum2(allRows2);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
         }
     }
-
-    public static void InitMyEnum(List<TableRow> rows) => InitList<MyEnum>(s_MyEnumData, rows);
-    public static void InitMyEnum2(List<TableRow> rows) => InitList<MyEnum2>(s_MyEnum2Data, rows);
-    public static int Get(MyEnum key) => s_MyEnumData[(int)key];
-    public static int Get(MyEnum2 key) => s_MyEnum2Data[(int)key];
 }
