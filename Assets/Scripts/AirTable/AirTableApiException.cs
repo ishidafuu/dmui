160694bc0aa1d177fe23824ad4030266169f433b
ishidafuu <ishidafuu@gmail.com>
2020-03-24 18:12:58 +0900

    update

--- Assets/Scripts/AirTable/AirTableApiException.cs
+++ Assets/Scripts/AirTable/AirTableApiException.cs
@@ -5,7 +5,8 @@ namespace DM
 {
     public abstract class AirTableApiException : Exception
     {
-        protected AirTableApiException(HttpStatusCode errorCode, string errorName, string errorMessage) : base($"{errorName} - {errorCode}: {errorMessage}")
+        protected AirTableApiException(HttpStatusCode errorCode, string errorName, string errorMessage) : base(
+            $"{errorName} - {errorCode}: {errorMessage}")
         {
             ErrorCode = errorCode;
             ErrorName = errorName;
@@ -13,29 +14,26 @@ namespace DM
         }
 
         public HttpStatusCode ErrorCode { get; private set; }
-        public string ErrorName{ get; private set; }
-        public string ErrorMessage{ get; private set; }
+        public string ErrorName { get; private set; }
+        public string ErrorMessage { get; private set; }
     }
 
     public class AirTableUnrecognizedException : AirTableApiException
     {
-        public AirTableUnrecognizedException(HttpStatusCode statusCode) : base(statusCode, "Unrecognized Error", $"AirTable returned HTTP status code {statusCode}")
-        {
-        }
+        public AirTableUnrecognizedException(HttpStatusCode statusCode) : base(statusCode, "Unrecognized Error",
+            $"AirTable returned HTTP status code {statusCode}") { }
     }
 
     public class AirTableBadRequestException : AirTableApiException
     {
-        public AirTableBadRequestException() : base(HttpStatusCode.BadRequest, "Bad Request", "The request encoding is invalid; the request can't be parsed as a valid JSON.")
-        {
-        }
+        public AirTableBadRequestException() : base(HttpStatusCode.BadRequest, "Bad Request",
+            "The request encoding is invalid; the request can't be parsed as a valid JSON.") { }
     }
 
     public class AirTableUnauthorizedException : AirTableApiException
     {
-        public AirTableUnauthorizedException() : base(HttpStatusCode.Unauthorized, "Unauthorized", "Accessing a protected resource without authorization or with invalid credentials.")
-        {
-        }
+        public AirTableUnauthorizedException() : base(HttpStatusCode.Unauthorized, "Unauthorized",
+            "Accessing a protected resource without authorization or with invalid credentials.") { }
     }
 
     public class AirTablePaymentRequiredException : AirTableApiException
@@ -43,9 +41,7 @@ namespace DM
         public AirTablePaymentRequiredException() : base(
             HttpStatusCode.PaymentRequired,
             "Payment Required",
-            "The account associated with the API key making requests hits a quota that can be increased by upgrading the AirTable account plan.")
-        {
-        }
+            "The account associated with the API key making requests hits a quota that can be increased by upgrading the AirTable account plan.") { }
     }
 
     public class AirTableForbiddenException : AirTableApiException
@@ -53,9 +49,7 @@ namespace DM
         public AirTableForbiddenException() : base(
             HttpStatusCode.Forbidden,
             "Forbidden",
-            "Accessing a protected resource with API credentials that don't have access to that resource.")
-        {
-        }
+            "Accessing a protected resource with API credentials that don't have access to that resource.") { }
     }
 
     public class AirTableNotFoundException : AirTableApiException
@@ -63,9 +57,7 @@ namespace DM
         public AirTableNotFoundException() : base(
             HttpStatusCode.NotFound,
             "Not Found",
-            "Route or resource is not found. This error is returned when the request hits an undefined route, or if the resource doesn't exist (e.g. has been deleted).")
-        {
-        }
+            "Route or resource is not found. This error is returned when the request hits an undefined route, or if the resource doesn't exist (e.g. has been deleted).") { }
     }
 
     public class AirTableRequestEntityTooLargeException : AirTableApiException
@@ -73,9 +65,7 @@ namespace DM
         public AirTableRequestEntityTooLargeException() : base(
             HttpStatusCode.RequestEntityTooLarge,
             "Request Entity Too Large",
-            "The request exceeded the maximum allowed payload size. You shouldn't encounter this under normal use.")
-        {
-        }
+            "The request exceeded the maximum allowed payload size. You shouldn't encounter this under normal use.") { }
     }
 
     public class AirTableInvalidRequestException : AirTableApiException
@@ -96,8 +86,6 @@ namespace DM
         public AirTableTooManyRequestsException() : base(
             (HttpStatusCode)429,
             "Too Many Requests",
-            "The user has sent too many requests in a given amount of time (rate limiting).")
-        {
-        }
+            "The user has sent too many requests in a given amount of time (rate limiting).") { }
     }
 }
