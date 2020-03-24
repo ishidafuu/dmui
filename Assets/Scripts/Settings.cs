40eedb60530e2f5864b92b356d0043589aec0258
ishidafuu <ishidafuu@gmail.com>
2020-03-24 17:13:49 +0900

    update

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+public static class Settings
+{
+    public enum MyEnum
+    {
+        Asdf,
+        Qwer,
+        Zxcv,
+    }
+
+    public enum MyEnum2
+    {
+        Asdf,
+        Qwer,
+        Zxcv,
+    }
+
+    public class TableRow
+    {
+        public string Name;
+        public int Value;
+    }
+
+    private static readonly List<int> s_MyEnumData = new List<int>();
+    private static readonly List<int> s_MyEnum2Data = new List<int>();
+
+    private static void InitList(ICollection<int> list, IEnumerable<TableRow> rows)
+    {
+        list.Clear();
+        foreach (TableRow row in rows)
+        {
+            list.Add(row.Value);
+        }
+    }
+
+    public static void InitMyEnum(IEnumerable<TableRow> rows) => InitList(s_MyEnumData, rows);
+    public static void InitMyEnum2(IEnumerable<TableRow> rows) => InitList(s_MyEnum2Data, rows);
+    public static int Get(MyEnum key) => s_MyEnumData[(int)key];
+    public static int Get(MyEnum2 key) => s_MyEnum2Data[(int)key];
+}
