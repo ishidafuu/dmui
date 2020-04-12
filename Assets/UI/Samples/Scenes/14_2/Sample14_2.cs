ff755766087d514f799f709d1e13ac6a8bb35de5
ishidafuu <ishidafuu@gmail.com>
2020-04-12 17:57:39 +0900

    firebase

--- Assets/UI/Samples/Scenes/14_2/Sample14_2.cs
+++ Assets/UI/Samples/Scenes/14_2/Sample14_2.cs
@@ -1,6 +1,12 @@
-﻿using DMUIFramework.Samples;
+﻿using System.Collections.Generic;
+using System.IO;
+using DMUIFramework.Samples;
 using UnityEngine;
 using UnityEngine.UI;
+using Firebase;
+using Firebase.Functions;
+using Firebase.Unity.Editor;
+using UniRx.Async;
 
 namespace DM
 {
@@ -10,6 +16,27 @@ namespace DM
         {
             UIController.SetImplement(new PrefabLoader(), new Sounder(), new FadeCreator(), new LoadingCreator(), new ToastCreator());
             UIController.Instance.AddFront(new Sample14_2Scene());
+            helloWorldOnCall();
+        }
+        
+        private async UniTask<string> helloWorldOnCall() {
+            // Create the arguments to the callable function.
+            var data = new Dictionary<string, object>();
+            data["name"] = "isdf";
+            data["asdf"] = "asdf";
+            
+            // FirebaseFunctions.DefaultInstance.UseFunctionsEmulator("http://localhost:5000");
+            HttpsCallableReference function = FirebaseFunctions.DefaultInstance.GetHttpsCallable("helloWorldOnCall");
+            HttpsCallableResult result = await function.CallAsync(data);
+
+            Debug.Log(result.Data);
+            var resDict = result.Data as Dictionary<object, object>;
+            foreach (var key in resDict.Keys)
+            {
+                Debug.Log(key.ToString());
+                Debug.Log(resDict[key.ToString()].ToString());
+            }
+            return "yeah";
         }
     }
 }
