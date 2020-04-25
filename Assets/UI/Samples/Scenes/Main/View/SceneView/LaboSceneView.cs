1d507006692180c82a6c67fcaf5a4ba75760ea10
ishidafuu <ishidafuu@gmail.com>
2020-04-25 15:29:21 +0900

    update

---
+++ Assets/UI/Samples/Scenes/Main/View/SceneView/LaboSceneView.cs
@@ -0,0 +1,13 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace DM
+{
+    public class LaboSceneView : MonoBehaviour
+    {
+        // [SerializeField] public HomeScrollerView m_HomeScrollerView;
+        // [SerializeField] public HomeScrollerController m_HomeScrollerController;
+    }
+}
