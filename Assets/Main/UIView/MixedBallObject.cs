dee2fca01cbe91525cf4b2c2cf1ffaaa6944d8a4
ishidafuu <ishidafuu@gmail.com>
2020-04-27 11:10:22 +0900

    update

---
+++ Assets/Main/UIView/MixedBallObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MixedBallObject : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image iconImage;
+    private Sprite nowSprite;
+
+    void Start()
+    {
+        nowSprite = null;
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        Debug.Log($"---MixedBallObject OnPointerEnter");
+        // if(pointerEventData.pointerDrag == null) return;
+        // Image droppedImage = pointerEventData.pointerDrag.GetComponent<Image>();
+        // iconImage.sprite = droppedImage.sprite;
+        // iconImage.color = Vector4.one * 0.6f;
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        Debug.Log($"---MixedBallObject OnPointerExit");
+        // if(pointerEventData.pointerDrag == null) return;
+        // iconImage.sprite = nowSprite;
+        // if(nowSprite == null)
+        //     iconImage.color = Vector4.zero;
+        // else
+        //     iconImage.color = Vector4.one;
+    }
+    public void OnDrop(PointerEventData pointerEventData)
+    {
+        Debug.Log($"---MixedBallObject OnDrop");
+        // Image droppedImage = pointerEventData.pointerDrag.GetComponent<Image>();
+        // iconImage.sprite = droppedImage.sprite;
+        // nowSprite = droppedImage.sprite;
+        // iconImage.color = Vector4.one;
+    }
+}
